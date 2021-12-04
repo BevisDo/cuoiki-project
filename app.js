851353f4b0ca98ea1c179d573959bd99494e08e3
lequoctrung-it <leqtrungit@gmail.com>
2021-12-04 20:58:05 +0700

    Add timeline template
    Add post template
    Add create post template

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
 var adminCreateRouter = require('./routes/admin-create');
+var timelineRouter = require('./routes/timeline');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
 app.use('/admin-create', adminCreateRouter);
+app.use('/timeline', timelineRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
