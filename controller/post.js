be4bae4090102297db6dfe3e30bff88db13d3592
BevisDo <doanhtuan52@gmail.com>
2021-12-07 12:52:51 +0700

    update
    Them sua bai viet
    them xoa bai viet

--- controller/post.js
+++ controller/post.js
@@ -1,38 +1,87 @@
+const pk_profile = require('../models/pk_profile')
 const Post = require('../models/Post')
 
-// class PostsController{
-//     create(req,res){
-//         new Post({
-//             content: req.body.content,
-//             createAt: new Date(),
-//         }).save(function(err){
-//             if(err){
-//                 console.log(err);
-//                 res.json({success: 'false'});
-//             }else{
-//                 res.json({success: 'true'});
-//             }
-//         })
-//     }
-// }
-// module.exports = new PostsController
 
-exports.postController = async(req,res) =>{
+// posts/create
+// Create Post
+exports.postCreateController = async(req,res) =>{
     const {content} = req.body
 
     if(!content)
-    return res.status(400).json({success: false, message: 'Chua nhap noi dung'})
+    return res.status(400).json({
+        success: false, 
+        message: 'Chua nhap noi dung'
+    })
 
     try {
         const newPost = new Post({
             user:req.userId,
             content
         })
-
         await newPost.save()
 
-        res.json({success: true, message: 'Dang thanh cong', post: newPost})
+        res.json({
+            success: true, 
+            message: 'Dang thanh cong', 
+            post: newPost
+        })
     } catch (error) {
         
     }
+}
+
+// posts/id_post
+// update post
+exports.postUpdateController =async(req,res) =>{
+    const {content} = req.body
+    
+    if(!content)
+    return res.status(400).json({
+        success: false, 
+        message: 'Chua nhap noi dung'
+    })
+
+    try {
+        let updatePost = {
+            content
+        }
+    const postUpdateCond = {_id: req.params.id, user: req.userId}
+
+    updatePost = await Post.findOneAndUpdate(postUpdateCond,updatePost,{new: true})
+
+    //khong dung user hoac khong dung id bai viet
+    if(!updatePost)
+    return res.status(401).json({
+        success: false, 
+        message:'Ko dung ng dung hoac khong co bai viet'
+    })
+
+    res.json({success: true, message: 'Da update',post: updatePost})
+    } catch (error) {
+        
+    }
+}
+
+//post/id_post
+//delete post
+exports.postDeleteController =async(req,res) =>{
+    try {
+        const postDeleteCond = {_id: req.params.id, user: req.userId}
+        const deletePost = await Post.findOneAndDelete(postDeleteCond)
+
+        if(!deletePost)
+        return res.status(401).json({
+            success: false, 
+            message:'Ko dung ng dung hoac khong co bai viet'
+        })
+
+        res.json({
+            success: true,
+            message: 'Da xoa bai viet',
+            post: deletePost
+        })
+    } catch (error) {
+        
+    }
+
 }
--- routes/post.js
+++ routes/post.js
@@ -2,10 +2,13 @@ var express = require('express')
 var router = express.Router();
 var verify = require('../lib/middleware/auth')
 
-var {postController}  = require('../controller/post')
-
-router.post('/create',verify, postController);
-
+var {postCreateController, postUpdateController, postDeleteController,}  = require('../controller/post')
+// POST post/create
+router.post('/create',verify,postCreateController);
+// PUT post/id_post
+router.put('/:id',verify,postUpdateController)
+// DELETE post/id_post
+router.delete('/:id',verify,postDeleteController)
 
 module.exports = router;
 
