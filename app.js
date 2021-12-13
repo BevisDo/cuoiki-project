26695449227eb918410f758315746ece17884fd2
Shyn002 <quangtri200028@gmail.com>
2021-12-13 21:28:59 +0700

    add List-PB

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var adminCreateRouter = require('./routes/admin-create');
 var timelineRouter = require('./routes/timeline');
 var notiPageRouter = require('./routes/noti-page');
 var changeInfoRouter = require('./routes/student-change-info');
-
+var ListPBRouter = require('./routes/list-PB');
 var app = express();
 
 // view engine setup
@@ -31,21 +31,21 @@ app.use('/admin-create', adminCreateRouter);
 app.use('/timeline', timelineRouter);
 app.use('/noti-page', notiPageRouter);
 app.use('/change-info', changeInfoRouter);
-
+app.use('/list-PB', ListPBRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
